b3aa95bd73edaa7015a297dbbdca0e7a8c6c645f
alexisgraff5 <lex.graff@gmail.com>
2017-02-24 12:57:17 -0700

    seeing if ava test is working yet

--- db/user_create_seed.sql
+++ db/user_create_seed.sql
@@ -1,7 +1,6 @@
 -- It may be helpful to drop and reinstantilize the table when doing
 -- the tests in case you delete users/cars the tests are expecting to see
-
-DROP TABLE IF EXISTS users;
+-- DROP TABLE IF EXISTS users;
 
 CREATE TABLE users 
 (
